37d6a0b59fb3f722d3e3b3e2b4a824b60868c920
MsdArul7 <arulnithish91@gmail.com>
2025-10-26 18:56:28 +0530

    Chnages done

--- Task3.py
+++ Task3.py
@@ -1,7 +1,7 @@
 # Using a for loop, generate a list of squares of only the odd numbers from 1 to 20.
 squares = []
 for num in range(1, 21):
-    if num % 3 != 0:
+    if num % 2 != 0:
         sq = num ** 2
         squares.append(sq)
 print("Squares of odd numbers from 1 to 20:", squares)
